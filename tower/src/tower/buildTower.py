2d8fc36c9362483fb1fe32bc1b5e446e2cc313e7
aonai <syuxiaolai@gmail.com>
2020-11-19 17:18:58 -0600

    added tolerance for table height

--- tower/src/tower/buildTower.py
+++ tower/src/tower/buildTower.py
@@ -5,12 +5,13 @@ class BuildTower():
     def __init__(self):
         rospy.loginfo("position info of building")
         self.TOL = 0.03 # tolerance for cup
+        self.TOL_table = 0.2 # tolerance for table
         #TODO: yaml
         self.radius = rospy.get_param("radius") + self.TOL # cup size
         self.height = rospy.get_param("length") + self.TOL
-        self.tableHeight =  rospy.get_param("t_z") + self.TOL # or the minimum height that the gripper should be allowed to go
+        self.tableHeight =  rospy.get_param("t_z") + self.TOL_table # or the minimum height that the gripper should be allowed to go
         self.centerY = 0.0
-        self.towerX = 1.0 #34.5 m
+        self.towerX = 1.0   #34.5 cm
 
     def tower_3_cups(self):
         rospy.loginfo("building tower with 3 cups")
