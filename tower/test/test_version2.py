671dd2cdf363d689959b176e09e3bedbefbb586a
Gabbie Wink <GabrielleWink2025@u.northwestern.edu>
2020-12-06 22:28:44 -0600

    added test folder and file

---
+++ tower/test/test_version2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+""" Create a unittest node that tests out python pacakege calc.py"""
+import unittest
+import rospy
+from tower.simulator import Scene
+
+
+
+
+
+class HardCaseNode(unittest.TestCase):
+
+    def test_zero_position(self):
+ 
+
+
+
+  
+
+
+    
+
+
+    
+
+if __name__ == "__main__":
+    import rosunit
+    rosunit.rosrun("tower", "Scene", Scene)  #class in simulator.py
+ 
